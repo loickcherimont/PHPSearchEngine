85999d9a80e52477ee0bdbc73c44b262b3d2a3e7
LoickDev97438 <loickcherimont@gmail.com>
2022-07-18 19:29:22 +0400

    Protect form against XSS attacker and restructure the project

--- index.php
+++ index.php
@@ -1,49 +1,79 @@
 <?php
-// PDO connection
+// Include required files
+include "process.php";
+
+
+// Required data to connect with PDO
+// to database countrypedia
+$hostname = "localhost";
+$dbname = "countrypedia";
+$dsn = sprintf("mysql:host=%s;dbname=%s;charset=utf8", $hostname, $dbname); // To avoid SQL injections
+$username = "root";
+$password = "";
+
+// Database connection
+// if failure, display an error
+// to the developer
 try {
-	$db = new PDO("mysql:host=localhost;dbname=countrypedia;charset=utf8", "root", "");
+	$pdo = new PDO($dsn, $username, $password);
 } catch(PDOException $e) {
 	echo $e->getMessage();
+	die();
 }
 
-// Form handling
-if (isset($_GET['search'])) {
-	$search = trim($_GET['query']);
-	$q = $db->prepare("SELECT * FROM countries WHERE title LIKE '%$search%'");
-	$q->execute(array());
-	$control = $q->fetchAll(PDO::FETCH_ASSOC);
-}
+// User submits a query
+if (isset($_GET["search"])) {
 
-if (empty($_GET['query'])) {
-	$q = $db->prepare("SELECT * FROM countries");
-	$q->execute(array());
-	$control = $q->fetchAll(PDO::FETCH_ASSOC);
-}
+	// Clean form
+	// and protect it
+	$q = cleanQuery($_GET["query"]);
 
+	// Get countries/y like this query
+	$query = $pdo->prepare("SELECT * FROM countries WHERE title LIKE '%$q%'");
+	$query->execute(array());
+	$countries = $query->fetchAll(PDO::FETCH_ASSOC);
+} 
+	
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>PHP Correction Search Engine</title>
+	<title>Search engine</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
-<body class="bg-primary">
-	<div class="container d-flex flex-column align-items-center justify-content-center text-white">
-		<h1 class="">Countrypedia</h1>
-		<p>Search engine for countries</p>
+<body>
+	<header class="container-fluid p-3">
+		<div class="row">
+			<h1 class="col bg-dark text-white text-center">Countrypedia</h1>
+		</div>
+	</header>
+
+	<!-- Main page: action protected by htmlspecialchars to avoid $_SERVER['PHP_SELF'] exploits -->
+	<div class="container-fluid d-flex flex-column align-items-center justify-content-center">
 		<form method="GET" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>">
-    		<input type="text" id="country" name="query" value="<?= htmlspecialchars($_GET['query']) ?? '' ?>">
+    		<input type="text" name="query">
   			<input type="submit" name="search" value="Search">
 		</form>
-		<h2><?= count($control) . " result(s):" ?></h2>
-		<ul class="list-group list-group-flush">
-			<?php foreach($control as $c): ?>
-				<li class="list-group-item bg-primary"><?= $c['title'] ?></li>
-			<?php endforeach; ?>
-		</ul>
+		<?php if(isset($_GET["search"])): ?>
+			<!-- Display results for the query -->
+			<h2><?= count($countries ?? array()) . " result(s) for <span style='color:red'>$q</span>" ?></h2>
+			<ul class="list-group list-group-flush">
+				<?php foreach($countries as $country): ?>
+					<li class="list-group-item fs-5"><?= $country["title"] ?></li>
+				<?php endforeach; ?>
+			</ul>
+		<?php endif; ?>
 	</div>
+
+	<!-- Developer signature -->
+	<footer class="container-fluid">
+		<div class="row">
+			<p class="col bg-dark text-white text-center">&copy; <a href="https://github.com/loickcherimont" class="link-white">Loick Cherimont</a> 2022</p>
+		</div>
+	</footer>
+    
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
